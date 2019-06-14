b271769023abdfd9af2d32e65e8d1c482e55babb
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-06-14 09:54:12 -0500

    Start working with just three main types of responses

--- src/DialogFlow/Payload/Google/OtherTypes.hs
+++ src/DialogFlow/Payload/Google/OtherTypes.hs
@@ -1,42 +1,5 @@
 module DialogFlow.Payload.Google.OtherTypes where
 
-data ActionType = UNKNOWN
-                | VIEW_DETAILS
-                | MODIFY
-                | CANCEL
-                | RETURN
-                | EXCHANGE
-                | EMAIl
-                | CALL
-                | REORDER
-                | REVIEW
-                | CUSTOMER_SERVICE
-                | FIX_ISSUE
-                deriving (Show)
-
-data PriceType = UNKNOWN_PRICE
-               | ESTIMATE
-               | ACTUAL
-               deriving (Show)
-data Money =
-  Money { currencyCode :: String
-        , units :: Int
-        , nanos :: Int -- int64 format
-        } deriving (Show)
-
-data Price = Price PriceType Money deriving Show
-
-data OrderState =
-  OrderState { osState :: String
-             , osLabel :: String
-             } deriving (Show)
-
-data LineItemUpdate =
-  LineItemUpdate { liuOrderState :: OrderState
-                 , liuPrice :: Price
-                 , liuReason :: String
-                 , liuExtension :: String -- fields of an arbitrary type
-                 } deriving (Show)
 data Image =
   Image { iUrl :: String
         , iAccessibilityText :: String
@@ -55,7 +18,3 @@ data MediaObject =
               , moLargeImage :: Image
               , moIcon :: Image
               } deriving Show
-
--- data Item =
---   Item { iTitle :: String
---        , i
--- src/DialogFlow/Payload/Google/Response.hs
+++ src/DialogFlow/Payload/Google/Response.hs
@@ -10,41 +10,11 @@ import DialogFlow.Payload.Google.OtherTypes
 
 data RichMessageType = RMTSimpleResponse
                      | RMTBasicCard
-                     | RMTStructuredResponse
                      | RMTMediaResponse
-                     | RMTCarouselBrowse
-                     | TableCard
-
-
-data Action =
-  Action { aType :: ActionType
-         , aButton :: M.BasicCardButton
-         } deriving (Show)
-
-data UserNotification =
-  UserNotification { unTitle :: String
-                   , unText :: String
-                   } deriving Show
-
-newtype Info = Info { unInfo :: String } deriving Show
-
-data OrderUpdate =
-  OrderUpdate { googleOrderId :: String
-              , actionOrderId :: String
-              , orderState :: OrderState
-              , orderManagementActions :: [Action]
-              , updateTime :: String -- Should be a timestamp
-              , totalPrice :: Price
-              , lineItemUpdates :: LineItemUpdate
-              , userNotification :: UserNotification
-              , infoExtension :: String -- some type object
-              , info :: Info
-              } deriving Show
 
 data Res t where
   SimpleResponse :: M.SimpleResponse -> Res 'RMTSimpleResponse
   BasicCard :: M.Msg 'M.MsgBasicCard -> Res 'RMTBasicCard
-  StructuredResponse :: OrderUpdate -> Res 'RMTStructuredResponse
   MediaResponse :: MediaType -> [MediaObject] -> Res 'RMTMediaResponse
 
 data RichResponse where
