0e73095373636b7aad04b9f48572904d304cf845
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-07-27 23:16:05 -0500

    Request docs

--- src/Dialogflow/Request.hs
+++ src/Dialogflow/Request.hs
@@ -3,6 +3,16 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
+{-|
+Module      : Dialogflow.Request
+Description : Dialogflow requests
+Copyright   : (c) Mauricio Fierro, 2019
+License     : BSD3-Clause
+Maintainer  : Mauricio Fierro <mauriciofierrom@gmail.com>
+
+This module contains types for Dialogflow webhook requests.
+-}
+
 module Dialogflow.Request where
 
 import Data.Aeson ( FromJSON
@@ -19,9 +29,10 @@ import GHC.Generics
 import qualified Data.Map as M
 import Data.List (find)
 
+-- | Represents an intent.
 data Intent =
-  Intent { intentName :: String -- ^ Intent name
-         , displayName :: String -- ^ Display name for the intent
+  Intent { intentName :: String  -- ^ Intent name.
+         , displayName :: String -- ^ Display name for the intent.
          } deriving (Eq, Generic, Show)
 
 instance FromJSON Intent where
@@ -30,14 +41,15 @@ instance FromJSON Intent where
     displayName <- i .: "displayName"
     return Intent {..}
 
+-- | Represents a context.
 data Context =
   Context { ctxName :: String
-          -- ^ The unique identifier of the context
+          -- ^ The unique identifier of the context.
           , ctxLifespanCount :: Maybe Int
           -- ^ The number of conversational query requests after which the
-          -- context expires
+          -- context expires.
           , ctxParameters :: M.Map String String
-          -- ^ The collection of parameters associated with this context
+          -- ^ The collection of parameters associated with this context.
           } deriving (Eq, Generic, Show)
 
 instance FromJSON Context where
@@ -53,42 +65,47 @@ instance ToJSON Context where
            , "lifespanCount" .= ctxLifespanCount
            , "parameters" .= ctxParameters ]
 
--- TODO: Include Messages when FromJSON instances are added
+-- | Represents the result of conversational query or event processing.
 data QueryResult =
   QueryResult { queryText :: String
-              -- ^ The original text of the query
+              -- ^ The original text of the query.
               , parameters :: M.Map String String
-              -- ^ Consists of parameter_name:parameter_value pairs
+              -- ^ Consists of parameter_name:parameter_value pairs.
               , allRequiredParamsPresent :: Bool
-              -- ^ Set to false if required parameters are missing in query
+              -- ^ Set to false if required parameters are missing in query.
               , fulfillmentText :: Maybe String
-              -- ^ Text to be pronounced to the user or shown on the screen
+              -- ^ Text to be pronounced to the user or shown on the screen.
               , outputContexts :: Maybe [Context]
-              -- ^ Collection of output contexts
+              -- ^ Collection of output contexts.
               , intent :: Maybe Intent
-              -- ^ The intent that matched the user's query
+              -- ^ The intent that matched the user's query.
               , intentDetectionConfidence :: Maybe Float
-              -- ^ Matching score for the intent
+              -- ^ Matching score for the intent.
               , diagnosticInfo :: Maybe (M.Map String String)
-              -- ^ Free-form diagnostic info
+              -- ^ Free-form diagnostic info.
               , languageCode :: String
-              -- ^ The language that was triggered during intent matching
+              -- ^ The language that was triggered during intent matching.
               } deriving (Generic, Show)
 
 instance FromJSON QueryResult
 
-data Request =
-  Request { responseId :: String
-          -- ^ Unique id for request
-          , session :: String
-          -- ^ Unique session id
-          , queryResult :: QueryResult
-          -- ^ Result of the conversation query or event processing
-          } deriving(Generic, Show)
+-- | The request message for a webhook call.
+data WebhookRequest =
+  WebhookRequest { responseId :: String
+                 -- ^ Unique id for request.
+                 , session :: String
+                 -- ^ Unique session id.
+                 , queryResult :: QueryResult
+                 -- ^ Result of the conversation query or event processing.
+                 } deriving(Generic, Show)
 
-instance FromJSON Request
+instance FromJSON WebhookRequest
 
-getContextParam :: [Context] -> String -> String -> Maybe String
+-- | Get a context parameter.
+getContextParam :: [Context] -- ^ Collection of contexts.
+                -> String    -- ^ Name of the context to get the param from.
+                -> String    -- ^ Name of the param to get from the context.
+                -> Maybe String
 getContextParam ctxs ctxName param = do
   mbCtx <- find (ctxByParam param) ctxs
   M.lookup param (ctxParameters mbCtx)
